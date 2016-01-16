db4bda9ab6438637e4d3e523db4e753fdf2edafa
Jayson <jayson.salkey@ufl.edu>
2016-01-15 19:05:48 -0500

    initial solution commit

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,15 +1,29 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here */
+  code: {type: String, required: true},
+  name: {type: String, required: true},
+  coordinates: {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String,
+  updated_at: Date,
+  created_at: Date
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  var doc = this;
+  doc.updated_at = (new Date()).toString().split(' ');
+  if(doc.created_at == ""){
+    doc.created_at = (new Date()).toString().split(' ');
+  }
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
--- queries.js
+++ queries.js
@@ -1,28 +1,60 @@
 /* Fill out these functions using Mongoose queries*/
+var mongoose = require('mongoose'),
+  Listing = require('./ListingSchema.js'),
+  config = require('./config');
+
+mongoose.connect(config.db.uri);
 
 var findLibraryWest = function() {
-  /* 
+  //find according to schema
+  Listing.findOne({name: 'Library West'}, function(err, data){
+    if(err) throw err;
+    console.log("Data of Library West"+"\n");
+    console.log(data+"\n");
+  });
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
 };
 var removeCable = function() {
+  Listing.findOne({code: 'CABL'}, function(err, data){
+    if(err) throw err;
+    console.log("Data of Cable TV"+"\n")
+    console.log(data+"\n");
+    data.remove(function(err){
+      if(err) throw err;
+      console.log("Deleted Listing!"+"\n");
+    });
+  });
+
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
 };
 var updatePhelpsMemorial = function() {
+
+  Listing.findOneAndUpdate({code: 'PHL'},{address: '100 Phelps Lab P.O. Box 116350 Gainesville, FL  32611'},{'new': true}, function(err, data){
+    if(err) throw err;
+    console.log("Updated PHL"+"\n")
+    console.log(data+"\n");
+  });
   /*
-    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+   Listing.find({}, function(err,data){
+     if(err) throw err;
+     console.log("All listings in the database"+"\n");
+     console.log(data+"\n");
+   })
 };
 
 findLibraryWest();
